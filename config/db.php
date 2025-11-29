0a635b5e1e2c065a314681889a9ed6ba31f18878
Muhammad-Alinoor <alinoor145387@outlook.com>
2025-11-29 18:15:33 +0600

    pdo options for security and performance

--- config/db.php
+++ config/db.php
@@ -7,3 +7,10 @@ define("DB_HOST","localhost");
 define("DB_NAME","smart-review-project");
 define("DB_USER","root");
 define("DB_PASS"," ");
+// pdo option for security and performance
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+];
