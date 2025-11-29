5d72e69da62759cac098594a2c8eb51e6dee02fb
Muhammad-Alinoor <alinoor145387@outlook.com>
2025-11-29 18:25:44 +0600

    error handling

--- config/db.php
+++ config/db.php
@@ -14,3 +14,18 @@ $options = [
     PDO::ATTR_EMULATE_PREPARES => false,
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
 ];
+try{
+    //create pdo isntance
+    $dsn = "mysql:host=". DB_HOST . ";
+    dbname=" . DB_NAME . ";
+    charset=utf8mb4";
+    $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+}
+catch(PDOException $e){
+    //log error and show user friendly message
+    error_log("Database connection error: " .  $e->getMessage());
+    die(json_encode([
+        "success" => "false",
+        "message" => "Database connection failed.Please contact adminstrator"
+    ]));
+}
